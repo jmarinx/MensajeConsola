27b24fe7d2e6549bf1fb1ae92c65a7e4d3703e4f
usuario <usuario@SV>
2022-03-29 10:34:43 +0200

    Modificamos mensaje salida pantalla.

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -3,7 +3,9 @@ public class ClasePrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Proyecto de trabajo con GITHUB versión 1 ");
+	
+		System.out.print("Es es una modificación de clase prueba");
+		
 	}
 
 }
